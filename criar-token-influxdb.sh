e4f5bee5ffbd80d0ca57212e5b6c82b68ebae34e
Thomas Patrick Burrow <thomas.burrow@ecomp.ufsm.br>
2026-05-15 13:55:04 -0300

    Muda referencias de podman para docker

--- criar-token-influxdb.sh
+++ criar-token-influxdb.sh
@@ -2,5 +2,4 @@
 set -a
 source .env #obtenha token
 set +a
-#podman ou docker?
-podman compose exec --env  influxdb influxdb3 create token --admin
+docker compose exec --env  influxdb influxdb3 create token --admin
--- inicializar-influxdb.sh
+++ inicializar-influxdb.sh
@@ -3,5 +3,4 @@ set -a
 source .env #obtenha token
 set +a
 export TOKEN=$INFLUXDB3_AUTH_TOKEN
-#podman ou docker?
-podman compose exec --env INFLUXDB3_AUTH_TOKEN=$TOKEN influxdb influxdb3 create database sensor
+docker compose exec --env INFLUXDB3_AUTH_TOKEN=$TOKEN influxdb influxdb3 create database sensor
